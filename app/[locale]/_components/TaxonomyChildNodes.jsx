6a4cde57918ca8359a29aa89cbe497773a5f74c9
ratigumashvili <ratigumashvili@gmail.com>
2024-10-30 14:15:09 +0400

    small fix

--- app/[locale]/_components/TaxonomyChildNodes.jsx
+++ app/[locale]/_components/TaxonomyChildNodes.jsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { Link } from "@/navigation"
-import { usePathname } from "next/navigation"
+import { Link, usePathname } from "@/navigation"
 
 export default function TaxonomyChildNodes({ data }) {
 
@@ -13,9 +12,7 @@ export default function TaxonomyChildNodes({ data }) {
             {data?.map(({ id, name, slug }) => (
                 <li key={id} className="px-4 py-4 mb-4 bg-slate-50 dark:bg-slate-600 rounded-md hover:bg-teal-700 hover:text-white transition-all ease-in">
                     <Link href={`${pathname}/${slug}`} className="flex justify-between">
-                        <span>
-                            {name}
-                        </span>
+                        {name}
                     </Link>
                 </li>
             ))}
--- app/[locale]/_components/TaxonomyParentImage.jsx
+++ app/[locale]/_components/TaxonomyParentImage.jsx
@@ -60,12 +60,12 @@ export default function TaxonomyParentImage({ url, title, id, taxon, taxonId, co
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-gray-800 dark:text-slate-400 p-6 text-left align-middle shadow-xl transition-all">
                                     <Dialog.Title
                                         as="h3"
                                         className="text-lg font-medium leading-6 text-gray-900"
                                     >
-                                        <div className='flex items-center justify-between'>
+                                        <div className='flex items-center justify-between dark:text-slate-400'>
                                             {title}
 
                                             <button onClick={closeModal}>
