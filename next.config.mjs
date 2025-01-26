89724a4cc47fdfb2a9c0149bee2bb91afedac148
ratigumashvili <ratigumashvili@gmail.com>
2025-01-26 11:08:25 +0400

    fixed decode downloaded font issue

--- next.config.mjs
+++ next.config.mjs
@@ -23,8 +23,8 @@
 //                 hostname: 'test-api.biodiversity.iliauni.edu.ge'
 //             }
 //         ],
-//     },
-// }
+//     }
+// };
 
 // export default withNextIntl(nextConfig);
 
@@ -34,26 +34,37 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'biodiversity.iliauni.edu.ge',
-            },
-            {
-                protocol: 'https',
-                hostname: 'api.biodiversity.iliauni.edu.ge'
-            },
-            {
-                protocol: 'http',
-                hostname: 'test-api.biodiversity.iliauni.edu.ge'
-            },
-            {
-                protocol: 'https',
-                hostname: 'test-api.biodiversity.iliauni.edu.ge'
-            }
-        ],
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'biodiversity.iliauni.edu.ge',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.biodiversity.iliauni.edu.ge',
+      },
+      {
+        protocol: 'http',
+        hostname: 'test-api.biodiversity.iliauni.edu.ge',
+      },
+      {
+        protocol: 'https',
+        hostname: 'test-api.biodiversity.iliauni.edu.ge',
+      },
+    ],
+  },
+  webpack: (config) => {
+    // Add a rule to handle font files
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/, // Match font file extensions
+      type: 'asset/resource', // Use Webpack's built-in asset module to serve font files
+      generator: {
+        filename: 'static/media/[hash][ext]', // Customize the output path and name
+      },
+    });
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
--- src/app/[locale]/_lib/fonts.js
+++ src/app/[locale]/_lib/fonts.js
@@ -3,19 +3,19 @@ import localfont from "next/font/local"
 export const firaGo = localfont({
     src: [
       {
-        path: "../../../../public/fonts/firago-300.ttf",
+        path: "../../../../public/fonts/firago-300.woff2",
         weight: "300"
       },
       {
-        path: "../../../../public/fonts/firago-400.ttf",
+        path: "../../../../public/fonts/firago-400.woff2",
         weight: "400"
       },
       {
-        path: "../../../../public/fonts/firago-500.ttf",
+        path: "../../../../public/fonts/firago-500.woff2",
         weight: "500"
       },
       {
-        path: "../../../../public/fonts/firago-700.ttf",
+        path: "../../../../public/fonts/firago-700.woff2",
         weight: "700"
       },
     ],
@@ -25,13 +25,13 @@ export const firaGo = localfont({
   export const arial = localfont({
     src: [
       {
-        path: '../../../../public/fonts/arial-regulart.ttf'
+        path: '../../../../public/fonts/arial-regulart.woff2'
       },
       {
-        path: '../../../../public/fonts/arial-medium.ttf'
+        path: '../../../../public/fonts/arial-medium.woff2'
       },
       {
-        path: '../../../../public/fonts/arial-bold.ttf'
+        path: '../../../../public/fonts/arial-bold.woff2'
       }
     ],
     variable: "--font-arial"
@@ -40,12 +40,12 @@ export const firaGo = localfont({
   export const bpg = localfont({
     src: [
         {
-            path: '../../../../public/fonts/bpg-nino-mtavruli-webfont.ttf',
+            path: '../../../../public/fonts/bpg-nino-mtavruli-webfont.woff2',
             weight: '400',
             style: 'normal'
         },
         {
-          path: '../../../../public/fonts/bpg_nino_mtavruli_bold.ttf',
+          path: '../../../../public/fonts/bpg_nino_mtavruli_bold.woff2',
           weight: '600',
           style: 'normal'
         }
