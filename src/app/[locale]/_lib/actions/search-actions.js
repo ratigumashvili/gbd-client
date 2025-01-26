d227c333f56e73314f121b0357089ef58dc55cde
ratigumashvili <ratigumashvili@gmail.com>
2025-01-26 22:02:02 +0400

    cleanup

--- src/app/[locale]/_lib/actions/search-actions.js
+++ src/app/[locale]/_lib/actions/search-actions.js
@@ -3,7 +3,7 @@ export async function handleSimpleSearch(formData) {
   const queryParams = {}
 
   if (formData.get("lge").trim() !== "") {
-      queryParams.name = formData.get("lge")
+    queryParams.name = formData.get("lge")
   }
 
   const queryParamString = new URLSearchParams(queryParams).toString()
@@ -12,25 +12,26 @@ export async function handleSimpleSearch(formData) {
 }
 
 export async function handleAdvancedSearch(formData) {
-    const queryParams = {};
 
-    const taxonRankValue = formData.get("taxon_rank")?.trim();
-    
-    if (taxonRankValue) {
-      queryParams.taxon_rank = taxonRankValue;
-    }
+  const queryParams = {};
 
-    const latinNameValue = formData.get("latin_name")?.trim();
-    if (latinNameValue) {
-      queryParams.latin_name = latinNameValue;
-    }
+  const taxonRankValue = formData.get("taxon_rank")?.trim();
 
-    const iucnValue = formData.get("iucn")?.trim();
-    if (iucnValue) {
-      queryParams.iucn = iucnValue;
-    }
-
-    const queryParamString = new URLSearchParams(queryParams).toString();
-
-    return queryParamString
+  if (taxonRankValue) {
+    queryParams.taxon_rank = taxonRankValue;
   }
+
+  const latinNameValue = formData.get("latin_name")?.trim();
+  if (latinNameValue) {
+    queryParams.latin_name = latinNameValue;
+  }
+
+  const iucnValue = formData.get("iucn")?.trim();
+  if (iucnValue) {
+    queryParams.iucn = iucnValue;
+  }
+
+  const queryParamString = new URLSearchParams(queryParams).toString();
+
+  return queryParamString
+}
