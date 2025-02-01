27cbbeabf879ebe71cbf90f8bf469d962a47efeb
ratigumashvili <ratigumashvili@gmail.com>
2025-02-01 23:16:56 +0400

    added skeleton to map

--- src/app/[locale]/_components/distribution_map/index.js
+++ src/app/[locale]/_components/distribution_map/index.js
@@ -1,7 +1,9 @@
 import dynamic from "next/dynamic";
+import SkeletonLoader from "@/src/app/[locale]/_components/SkeletonLoader"
 
 const Map = dynamic(() => import('./Distribution'), {
-    ssr: false
+    ssr: false,
+    loading: () => <SkeletonLoader classNames={"w-full h-[450px] mt-12"} />
 })
 
 export default Map
