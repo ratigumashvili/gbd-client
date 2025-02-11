7029874232237a8833f92c8f0c5ae00b57455bb8
ratigumashvili <ratigumashvili@gmail.com>
2025-02-11 14:03:54 +0400

    small fix

--- src/app/[locale]/_components/TaxonomyGallerySearch.jsx
+++ src/app/[locale]/_components/TaxonomyGallerySearch.jsx
@@ -1,21 +1,24 @@
 "use client"
 
+import { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { useTranslations } from 'next-intl'
 import { usePathname, useRouter } from '@/src/i18n/routing'
 
 function TaxonomyGallerySearch({ query }) {
 
+    const [searchValue, setSearchValue] = useState("")
+
     const searchParams = useSearchParams()
     const pathname = usePathname()
     const router = useRouter()
 
     async function handleFormSubmit(formData) {
-        const latinName = formData.get("latinName")
+        // const latinName = formData.get("latinName")
 
         const newParams = new URLSearchParams(searchParams);
         newParams.set("page", 1);
-        newParams.set("q", latinName);
+        newParams.set("q", searchValue);
 
         router.replace(`${pathname}?${newParams.toString()}`)
 
@@ -37,13 +40,15 @@ function TaxonomyGallerySearch({ query }) {
                 <input
                     type="text"
                     name="latinName"
-                    defaultValue={query}
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
                     className="px-3 py-2 border rounded-sm"
                     placeholder={t("latin_name")}
                 />
                 <button
                     type="submit"
-                    className="button-secondary"
+                    className="button-secondary disabled:opacity-50 pointer-events-none"
+                    disabled={searchValue.trim() === ""}
                 >
                     {t("submit")}
                 </button>
