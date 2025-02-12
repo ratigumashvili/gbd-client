eb50f555b21ad3935f65a0f7622ead12ee1c6fe4
ratigumashvili <ratigumashvili@gmail.com>
2025-02-12 14:40:04 +0400

    cleanup

--- src/app/[locale]/_components/TaxonomyParentNodes.jsx
+++ src/app/[locale]/_components/TaxonomyParentNodes.jsx
@@ -6,10 +6,11 @@ import { Link, usePathname } from "@/src/i18n/routing"
 import { filterTaxonValue, formatPathname, separator } from "@/src/app/[locale]/_lib/helpers"
 
 export default function TaxonomyParentNodes({ data }) {
+    
     const path = usePathname()
+
     return (
         <>
-            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
             {data && data?.length !== 0 && (
                 <div className="py-4 mt-8 flex items-center gap-2 flex-wrap text-base border-b border-b-slate-100 border-t border-t-slate-50 mb-10">
                     <Image src={'/nodes.svg'} alt="Nodes" width={20} height={20} />
