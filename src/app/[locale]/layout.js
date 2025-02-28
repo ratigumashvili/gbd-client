6dc3a7c8018bbed5a2c8eda68405ecddd5a5852d
ratigumashvili <ratigumashvili@gmail.com>
2025-02-28 12:07:30 +0400

    added featured image

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -34,7 +34,7 @@ export async function generateMetadata({ params }) {
       icon: ['/favicon.ico'],
     },
     openGraph: {
-      images: ['']
+      images: ['/featured_image.png']
     },
   }
 
